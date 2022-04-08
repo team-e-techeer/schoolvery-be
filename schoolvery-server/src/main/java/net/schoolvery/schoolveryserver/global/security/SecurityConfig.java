1b5183950bdfab156e3505254681345abb7e3566
Judy <judyb0626@gmail.com>
2022-04-08 11:49:46 -0700

    fix: Disable spring security default login page

--- schoolvery-server/src/main/java/net/schoolvery/schoolveryserver/global/security/SecurityConfig.java
+++ schoolvery-server/src/main/java/net/schoolvery/schoolveryserver/global/security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package net.schoolvery.schoolveryserver.global.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
