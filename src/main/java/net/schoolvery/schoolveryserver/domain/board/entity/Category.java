480e0b395ddf566579847bd022490c20eba03189
ksy990628 <ksy990628@gmail.com>
2022-04-18 22:40:33 +0900

    chore: Update category

--- src/main/java/net/schoolvery/schoolveryserver/domain/board/entity/Category.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/board/entity/Category.java
@@ -1,21 +1,28 @@
 package net.schoolvery.schoolveryserver.domain.board.entity;
 
+import lombok.Builder;
 import net.schoolvery.schoolveryserver.global.common.BaseEntity;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
-import java.util.UUID;
 @Entity
+@Table(name = "category")
+@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Category extends BaseEntity{
+    /*
     @Id
     @GeneratedValue(generator = "uuid2") //@GeneratedValue -> PK 설정
     @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //@GeneratedValue -> PK 설정, GenerationType.IDENTITY -> AutoIncrement
+    private Integer id;
 
     @Column(nullable = false)
     private String name;
