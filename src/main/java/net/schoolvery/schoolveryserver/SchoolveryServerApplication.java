3084b60a046adfb22df816a26e3638b9634e3917
ksy990628 <ksy990628@gmail.com>
2022-04-19 00:24:10 +0900

    Add JPA Auditing

--- src/main/java/net/schoolvery/schoolveryserver/SchoolveryServerApplication.java
+++ src/main/java/net/schoolvery/schoolveryserver/SchoolveryServerApplication.java
@@ -2,8 +2,9 @@ package net.schoolvery.schoolveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
+@EnableJpaAuditing
 public class SchoolveryServerApplication {
 
 	public static void main(String[] args) {
