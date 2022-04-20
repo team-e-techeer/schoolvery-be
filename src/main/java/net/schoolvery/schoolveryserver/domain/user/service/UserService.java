f4bf33b72ac8b1fbe862c2d509d54daa00d5a4d2
Joe <at365@naver.com>
2022-04-20 23:16:41 +0900

    feat:Update User Service

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
@@ -1,7 +1,45 @@
 package net.schoolvery.schoolveryserver.domain.user.service;
 
-import org.springframework.stereotype.Service;
+import net.schoolvery.schoolveryserver.domain.user.dto.request.UserCreateRequestDto;
+import net.schoolvery.schoolveryserver.domain.user.dto.response.UserCreateResponseDto;
+import net.schoolvery.schoolveryserver.domain.user.entity.User;
 
-@Service
+import java.util.List;
+
 public interface UserService {
+
+    default User createUserRequest(UserCreateRequestDto userCreateRequestDto) {
+        User user = User.builder()
+                .name(userCreateRequestDto.getName())
+                .nickname(userCreateRequestDto.getNickname())
+                .email(userCreateRequestDto.getEmail())
+                .password(userCreateRequestDto.getPassword())
+                .school_num(userCreateRequestDto.getSchool_num())
+                .school_id(userCreateRequestDto.getSchool_id())
+                .build();
+
+        return user;
+    }
+
+    default UserCreateResponseDto createUserResponse(User user) {
+        UserCreateResponseDto userDto = UserCreateResponseDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .school_num(user.getSchool_num())
+                .school_id(user.getSchool_id())
+                .build();
+
+        return userDto;
+
+    }
+
+    public List<User> findAllUser();
+
+    public String delete(String idx);
+
+
+
 }
--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserServiceImpl.java
@@ -1,7 +1,48 @@
 package net.schoolvery.schoolveryserver.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
+import net.schoolvery.schoolveryserver.domain.user.dto.request.UserCreateRequestDto;
+import net.schoolvery.schoolveryserver.domain.user.dto.response.UserCreateResponseDto;
+import net.schoolvery.schoolveryserver.domain.user.entity.User;
+import net.schoolvery.schoolveryserver.domain.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserServiceImpl {
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    // User Create
+    public UserCreateResponseDto createUser(UserCreateRequestDto userCreateRequestDto) {
+
+        User user = createUserRequest(userCreateRequestDto);
+
+        return createUserResponse(userRepository.save(user));
+    }
+
+    @Override
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public String delete(String idx) {
+        String result = userRepository.delete(idx);
+
+        return null;
+    }
+
+    // User Read
+
+
+
+
+
+
+
 }
