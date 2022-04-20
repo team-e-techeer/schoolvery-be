ca7c8a68cde878192773e6b57eb35f67761cd50b
Joe <at365@naver.com>
2022-04-20 23:16:28 +0900

    feat:update User Repository

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,String> {
+
+    String delete(String idx);
 
 }
