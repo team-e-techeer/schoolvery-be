9e2f9db3c9412d73d7cf85530179ea6c95f8eccc
Judy <judyb0626@gmail.com>
2022-04-19 22:34:12 -0700

    feat: Add global exception

--- src/main/java/net/schoolvery/schoolveryserver/global/error/ErrorResponse.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/error/ErrorResponse.java
@@ -57,4 +57,34 @@ public class ErrorResponse {
   }
 
 
+  @Getter
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  public static class FieldError {
+    private String field;
+    private String value;
+    private String reason;
+
+    private FieldError(final String field, final String value, final String reason) {
+      this.field = field;
+      this.value = value;
+      this.reason = reason;
+    }
+
+    public static List<FieldError> of(final String field, final String value, final String reason) {
+      List<FieldError> fieldErrors = new ArrayList<>();
+      fieldErrors.add(new FieldError(field, value, reason));
+      return fieldErrors;
+    }
+
+    private static List<FieldError> of(final BindingResult bindingResult) {
+      final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+      return fieldErrors.stream()
+          .map(error -> new FieldError(
+              error.getField(),
+              error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+              error.getDefaultMessage()))
+          .collect(Collectors.toList());
+    }
+  }
+
 }
--- src/main/java/net/schoolvery/schoolveryserver/global/error/exception/ErrorCode.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/error/exception/ErrorCode.java
@@ -14,14 +14,9 @@ public enum ErrorCode {
   INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
   HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
 
-
   // Member
   EMAIL_DUPLICATION(400, "M001", "Email is Duplication"),
   LOGIN_INPUT_INVALID(400, "M002", "Login input is invalid"),
-
-  // Coupon
-  COUPON_ALREADY_USE(400, "CO001", "Coupon was already used"),
-  COUPON_EXPIRE(400, "CO002", "Coupon was already expired")
 
   ;
   private final String code;
