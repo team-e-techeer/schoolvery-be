26712b01d228dd8b0f781f74f47e63d9d2fdaa62
Joe <at365@naver.com>
2022-04-23 23:50:38 +0900

    feat:Update UserRepository

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package net.schoolvery.schoolveryserver.domain.user.repository;
 
-import net.schoolvery.schoolveryserver.domain.user.dto.response.GetUserResponseDto;
 import net.schoolvery.schoolveryserver.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
