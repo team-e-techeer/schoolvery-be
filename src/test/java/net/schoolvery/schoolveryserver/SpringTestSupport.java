318e8971c622d840716311de579ea487d4017294
Joe <at365@naver.com>
2022-04-30 00:00:16 +0900

    feat:Add SpringTest Support

--- src/test/java/net/schoolvery/schoolveryserver/SpringTestSupport.java
+++ src/test/java/net/schoolvery/schoolveryserver/SpringTestSupport.java
@@ -1,2 +1,21 @@
-package net.schoolvery.schoolveryserver.domain.hello.service;public class SpringTestSupport {
+package net.schoolvery.schoolveryserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@AutoConfigureMockMvc
+public class SpringTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper mapper;
+
 }
