e44b2b79aeaab7bf43a48d125dd431dff85e3832
Joe <at365@naver.com>
2022-04-29 15:33:14 +0900

    feat:Add findbyEmail in UserRepository

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -12,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, UUID>, QuerydslPredi
 
     void deleteAllById(UUID id);
 
+    Optional<User> findByEmail(String email);
 }
