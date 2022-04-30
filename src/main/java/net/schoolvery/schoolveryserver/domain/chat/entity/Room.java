21916b6496cec934f6345f891401477178b7fc46
ksy990628 <ksy990628@gmail.com>
2022-05-01 01:32:35 +0900

    feat: Add room entity

---
+++ src/main/java/net/schoolvery/schoolveryserver/domain/chat/entity/Room.java
@@ -0,0 +1,32 @@
+package net.schoolvery.schoolveryserver.domain.chat.entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import net.schoolvery.schoolveryserver.global.common.BaseEntity;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+@Entity
+@Table(name = "room")
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room extends BaseEntity {
+    @Id
+    @GeneratedValue(generator = "UUID",strategy = GenerationType.IDENTITY)
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "uuid2"
+    )
+    private UUID id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+//    public void update(String name) {
+//        this.name = name;
+//    }
+}
