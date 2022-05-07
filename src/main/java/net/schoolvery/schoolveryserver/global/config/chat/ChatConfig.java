76638b1f275bb130189c29d2a58feb557234db5e
ksy990628 <ksy990628@gmail.com>
2022-05-08 02:28:24 +0900

    chore: Update chat config

--- src/main/java/net/schoolvery/schoolveryserver/global/config/chat/ChatConfig.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/config/chat/ChatConfig.java
@@ -11,14 +11,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class ChatConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
-        registry.enableSimpleBroker("/queue", "/topic");
-
+        registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
