6f90b69f68fce02d71d84284737915ecc1e73833
Joe <at365@naver.com>
2022-05-11 23:23:02 +0900

    Update UserServiceImpl.java

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserServiceImpl.java
@@ -44,62 +44,6 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private final JwtTokenProvider jwtTokenProvider;
 
-    @Autowired
-    private final JavaMailSender emailSender;
-
-    public static final String ePw = createKey();
-
-    private MimeMessage createMessage(String to) throws Exception {
-
-        MimeMessage message = emailSender.createMimeMessage();
-
-        message.addRecipients(Message.RecipientType.TO, to);
-        message.setSubject("Schoolvery회원가입 이메일 인증");
-
-        String msgg = "";
-        msgg+= "<div style='margin:100px;'>";
-        msgg+= "<h1> 안녕하세요 Schoolvery입니다. </h1>";
-        msgg+= "<br>";
-        msgg+= "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다!<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= ePw+"</strong><div><br/> ";
-        msgg+= "</div>";
-        message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("ats36574@gmail.com","schoolvery"));//보내는 사람
-
-        return message;
-    }
-
-    public static String createKey() {
-        StringBuffer key = new StringBuffer();
-        Random rnd = new Random();
-
-        for (int i = 0; i < 8; i++) {
-            int idx = rnd.nextInt(3);
-
-            switch (idx) {
-                case 0:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    break;
-                case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    break;
-                case 2:
-                    key.append((rnd.nextInt(10)));
-                    break;
-
-            }
-        }
-
-        return key.toString();
-    }
-
 
     // User Create
     @Override
