349721ae417a9590440ad53769d422a7ddd14cb1
Joe <at365@naver.com>
2022-05-28 01:16:16 +0900

    feat:Add School Service

---
+++ src/main/java/net/schoolvery/schoolveryserver/domain/school/SchoolService.java
@@ -0,0 +1,35 @@
+package net.schoolvery.schoolveryserver.domain.school;
+
+import net.schoolvery.schoolveryserver.domain.school.Dto.request.SchoolRequestDto;
+import net.schoolvery.schoolveryserver.domain.school.Dto.response.SchoolResponseDto;
+import net.schoolvery.schoolveryserver.domain.school.entity.School;
+import net.schoolvery.schoolveryserver.global.common.dto.PageRequestDto;
+import net.schoolvery.schoolveryserver.global.common.dto.PageResultDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SchoolService {
+    SchoolResponseDto createSchool(SchoolRequestDto schoolRequestDto);
+    void deleteSchool(UUID id);
+    SchoolResponseDto updateSchool(UUID id, SchoolRequestDto requestDto);
+    SchoolResponseDto getSchoolById(UUID id);
+   List<School> getAllSchool(School school);
+
+    default School createSchoolRequest(SchoolRequestDto schoolRequestDto) {
+        School school = School.builder()
+                .id(schoolRequestDto.getId())
+                .schoolName(schoolRequestDto.getSchoolName())
+                .build();
+
+        return school;
+    }
+
+    default SchoolResponseDto createSchoolResponse(School school) {
+
+        return SchoolResponseDto.builder()
+                .id(school.getId())
+                .schoolName(school.getSchoolName())
+                .build();
+    }
+}
---
+++ src/main/java/net/schoolvery/schoolveryserver/domain/school/SchoolServiceImpl.java
@@ -0,0 +1,66 @@
+package net.schoolvery.schoolveryserver.domain.school;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import net.schoolvery.schoolveryserver.domain.school.Dto.request.SchoolRequestDto;
+import net.schoolvery.schoolveryserver.domain.school.Dto.response.SchoolResponseDto;
+import net.schoolvery.schoolveryserver.domain.school.entity.School;
+import net.schoolvery.schoolveryserver.domain.school.repository.SchoolRepository;
+import net.schoolvery.schoolveryserver.global.common.dto.PageRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+@Transactional
+public class SchoolServiceImpl implements SchoolService{
+
+    @Autowired
+    private final SchoolRepository schoolRepository;
+
+    @Override
+    public SchoolResponseDto createSchool(SchoolRequestDto schoolRequestDto) {
+
+        School school = createSchoolRequest(schoolRequestDto);
+        schoolRepository.save(school);
+
+        return createSchoolResponse(school);
+    }
+
+    @Override
+    public void deleteSchool(UUID id) {
+        schoolRepository.deleteById(id);
+    }
+
+    @Override
+    public SchoolResponseDto updateSchool(UUID id, SchoolRequestDto requestDto) {
+        Optional<School> school = schoolRepository.findById(id);
+        School entity = school.get();
+        entity.update(
+                requestDto.getSchoolName()
+        );
+        School updateEntity = schoolRepository.save(entity);
+
+        return createSchoolResponse(updateEntity);
+    }
+
+    @Override
+    public SchoolResponseDto getSchoolById(UUID id) {
+        Optional<School> result = schoolRepository.findById(id);
+        return result.isPresent() ? createSchoolResponse(result.get()) : null;
+    }
+
+    @Override
+    public List<School> getAllSchool(School school) {
+
+        return schoolRepository.findAll();
+    }
+
+}
