d9cbce0244b7408bbc6d505aec80abf4c86b872e
Joe <at365@naver.com>
2022-06-01 19:49:41 +0900

    feat:Update school req / res Dto

--- src/main/java/net/schoolvery/schoolveryserver/domain/school/Dto/request/SchoolRequestDto.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/school/Dto/request/SchoolRequestDto.java
@@ -1,16 +1,16 @@
 package net.schoolvery.schoolveryserver.domain.school.Dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SchoolRequestDto {
 
-    private UUID id;
+    private UUID schoolId;
     private String schoolName;
 }
