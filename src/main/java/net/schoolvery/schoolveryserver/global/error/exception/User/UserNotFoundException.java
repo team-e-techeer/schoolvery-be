df837f7c69461f361151ee8c79d8f2399a13d01e
Joe <at365@naver.com>
2022-06-13 19:25:03 +0900

    feat:Add User + Email Exception

--- src/main/java/net/schoolvery/schoolveryserver/global/error/exception/User/UserNotFoundException.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/error/exception/User/UserNotFoundException.java
@@ -1,5 +1,12 @@
 package net.schoolvery.schoolveryserver.global.error.exception.User;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
