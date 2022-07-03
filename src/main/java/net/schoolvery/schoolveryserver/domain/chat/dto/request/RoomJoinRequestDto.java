caaaa8956362352d69eac6d23beddbabb5f6970a
Joe <at365@naver.com>
2022-07-03 19:13:32 +0900

    feat:Add RoomJoin Req/Res Dto

---
+++ src/main/java/net/schoolvery/schoolveryserver/domain/chat/dto/request/RoomJoinRequestDto.java
@@ -0,0 +1,19 @@
+package net.schoolvery.schoolveryserver.domain.chat.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomJoinRequestDto {
+
+    private UUID member_id;
+    private UUID room_id;
+
+}
---
+++ src/main/java/net/schoolvery/schoolveryserver/domain/chat/dto/response/RoomJoinResponseDto.java
@@ -0,0 +1,18 @@
+package net.schoolvery.schoolveryserver.domain.chat.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomJoinResponseDto {
+
+    private UUID room_id;
+
+}
