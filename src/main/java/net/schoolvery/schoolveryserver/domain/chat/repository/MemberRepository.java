f1a14c8670fc6f09f8f05790d83379d6f575d151
Joe <at365@naver.com>
2022-07-04 17:29:47 +0900

    feat:Add ORM query in MemberRepository

--- src/main/java/net/schoolvery/schoolveryserver/domain/chat/repository/MemberRepository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/chat/repository/MemberRepository.java
@@ -2,7 +2,11 @@ package net.schoolvery.schoolveryserver.domain.chat.repository;
 
 import net.schoolvery.schoolveryserver.domain.chat.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
+
+    Optional<Member> findByMemberId(UUID member_id);
 }
