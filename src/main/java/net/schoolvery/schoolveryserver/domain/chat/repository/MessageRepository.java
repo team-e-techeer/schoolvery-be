d074496c610f6af59ed2c497f49bc5131b967ca2
Joe <at365@naver.com>
2022-07-09 22:15:28 +0900

    feat: Add findByRoomId in Message Repository

--- src/main/java/net/schoolvery/schoolveryserver/domain/chat/repository/MessageRepository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/chat/repository/MessageRepository.java
@@ -1,7 +1,13 @@
 package net.schoolvery.schoolveryserver.domain.chat.repository;
+import net.schoolvery.schoolveryserver.domain.chat.entity.Member;
 import net.schoolvery.schoolveryserver.domain.chat.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
+
+    List<Message> findByRoomId(UUID id);
+
 }
