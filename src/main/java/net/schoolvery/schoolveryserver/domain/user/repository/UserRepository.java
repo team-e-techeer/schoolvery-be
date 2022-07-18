4d4fff138ce75f4e74f121f4b3eab2e7ff11f356
Joe <at365@naver.com>
2022-07-18 23:28:21 +0900

    feat:Update UserRepository

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package net.schoolvery.schoolveryserver.domain.user.repository;
 
 import net.schoolvery.schoolveryserver.domain.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,10 @@ public interface UserRepository extends JpaRepository<User, UUID>, QuerydslPredi
 
     void deleteAllById(UUID id);
 
+    // 쿼리가 수행될때 Lazy 조회가 아닌 Eager 조회로 authorities정보도 같이 가져옴
+    @EntityGraph(attributePaths = "authorities")
     Optional<User> findByEmail(String email);
 
+    @EntityGraph(attributePaths = "authorities")
     Optional<User> findByNickname(String nickname);
 }
