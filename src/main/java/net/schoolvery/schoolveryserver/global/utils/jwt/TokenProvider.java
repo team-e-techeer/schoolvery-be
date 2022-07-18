2aa4ca57cc90c8ca43a9f9b3a371b1f1cd7af566
Joe <at365@naver.com>
2022-07-18 23:29:40 +0900

    feat:Add JwtTokenProvider

---
+++ src/main/java/net/schoolvery/schoolveryserver/global/utils/jwt/TokenProvider.java
@@ -0,0 +1,105 @@
+package net.schoolvery.schoolveryserver.global.utils.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import net.schoolvery.schoolveryserver.global.error.exception.BusinessException;
+import net.schoolvery.schoolveryserver.global.error.exception.ErrorCode;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import org.springframework.stereotype.Component;
+import org.webjars.NotFoundException;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import static net.schoolvery.schoolveryserver.global.error.exception.ErrorCode.*;
+
+@Component
+@Slf4j
+public class TokenProvider implements InitializingBean {
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+
+    private Key key;
+
+    public TokenProvider(
+            @Value("${JWT_SECRET_KEY}") String secret,
+            @Value("${JWT_EXPIRE_TIME}") long tokenValidityInMilliseconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // Authentication 객체 권한 정보를 이용해 토큰 생성하는 메서드
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    //Token에 담겨있는 정보를 이용해 Authentication 객체를 리턴하는 메서드\
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    //토큰의 유효성 검증 수행
+    // 토큰 파싱후, 발생하는 Exception 캐치, 문제 생길시 throw exception 정상일시 true
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | NotFoundException exception) {
+            throw new BusinessException(NOTFOUND_JWT);
+        } catch (ExpiredJwtException e) {
+            throw new BusinessException(EXPIRED_JWT);
+        } catch (UnsupportedJwtException e) {
+            throw new BusinessException(UNSUPPORT_JWT);
+        } catch (IllegalArgumentException e) {
+            throw new BusinessException(Illegal_JWT);
+        }
+    }
+
+}
