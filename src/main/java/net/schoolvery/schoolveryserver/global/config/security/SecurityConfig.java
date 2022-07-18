01ecae221ce0d3afc6f5da5671862b93e85a9a8f
Joe <at365@naver.com>
2022-07-18 23:28:36 +0900

    feat:Update SecurityConfig

--- src/main/java/net/schoolvery/schoolveryserver/global/config/security/SecurityConfig.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/config/security/SecurityConfig.java
@@ -1,10 +1,16 @@
 package net.schoolvery.schoolveryserver.global.config.security;
 
+import net.schoolvery.schoolveryserver.global.utils.jwt.JwtSecurityConfig;
+import net.schoolvery.schoolveryserver.global.utils.jwt.TokenProvider;
+import net.schoolvery.schoolveryserver.global.utils.jwt.error.JwtAccessDeniedHandler;
+import net.schoolvery.schoolveryserver.global.utils.jwt.error.JwtAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,7 +21,20 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드 단위로 추가하기 위한 적용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(TokenProvider tokenProvider,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -24,12 +43,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
-                .csrf().disable()
+        http
+                .csrf().disable() // 토큰 사용하기에 disable
+
+                // Exception문들 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler).and()
+
+                .headers()
+                .frameOptions()
+                .sameOrigin().and()
+
+                // 세션사용을 따로하지 않기에, STATELESS 설정
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .cors().configurationSource(corsConfigurationSource()).and()
-                .formLogin().disable()
-                .headers().frameOptions().disable();
+
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에대한 접근 제한 설정
+                .antMatchers("/api/v1/users/**").permitAll() // /api/v1/users 에관한 요청은 인증없이 접근 허용
+                .antMatchers("/api/v1/school/**").permitAll()
+                .antMatchers("/api/v1/categories/**").permitAll()
+                .anyRequest().authenticated() // 나머지 요청들은 전부 인증을 받아야한다.
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
     }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
