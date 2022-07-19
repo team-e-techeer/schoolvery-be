0bee37181fc8af88d71959570c0cafa569b6266f
Ueibin <ats1113@zuminternet.com>
2022-07-19 14:47:13 +0900

    feat:Add Cors Security & API Security

---
+++ src/main/java/net/schoolvery/schoolveryserver/global/config/security/CorsConfig.java
@@ -0,0 +1,25 @@
+package net.schoolvery.schoolveryserver.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/api/v1/**", config);
+        return new CorsFilter(source);
+    }
+
+}
--- src/main/java/net/schoolvery/schoolveryserver/global/config/security/SecurityConfig.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/config/security/SecurityConfig.java
@@ -4,8 +4,10 @@ import net.schoolvery.schoolveryserver.global.utils.jwt.JwtSecurityConfig;
 import net.schoolvery.schoolveryserver.global.utils.jwt.TokenProvider;
 import net.schoolvery.schoolveryserver.global.utils.jwt.error.JwtAccessDeniedHandler;
 import net.schoolvery.schoolveryserver.global.utils.jwt.error.JwtAuthenticationEntryPoint;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,26 +15,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Arrays;
-
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드 단위로 추가하기 위한 적용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
 
-    public SecurityConfig(TokenProvider tokenProvider,
-                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler, CorsFilter corsFilter) {
         this.tokenProvider = tokenProvider;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.corsFilter = corsFilter;
     }
 
 
@@ -42,9 +40,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/api/v1/users/**", "/api/v1/school", "/api/v1/categories/**"); // /api/v1/users에 관한 요청은 인증없이 접근 허용
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() // 토큰 사용하기에 disable
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
                 // Exception문들 추가
                 .exceptionHandling()
@@ -58,12 +65,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 세션사용을 따로하지 않기에, STATELESS 설정
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .cors().configurationSource(corsConfigurationSource()).and()
 
                 .authorizeRequests() // HttpServletRequest를 사용하는 요청들에대한 접근 제한 설정
-                .antMatchers("/api/v1/users/**").permitAll() // /api/v1/users 에관한 요청은 인증없이 접근 허용
-                .antMatchers("/api/v1/school/**").permitAll()
-                .antMatchers("/api/v1/categories/**").permitAll()
                 .anyRequest().authenticated() // 나머지 요청들은 전부 인증을 받아야한다.
 
                 .and()
@@ -71,14 +74,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT","DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
