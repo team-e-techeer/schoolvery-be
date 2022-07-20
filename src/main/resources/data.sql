7525ec761747e0ff2bdf016c6ed5db218f7af136
Ueibin <ats1113@zuminternet.com>
2022-07-20 18:41:13 +0900

    hotfix:Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,6 +16,5 @@ INSERT INTO school (school_id, school_name) values (UNHEX(REPLACE(UUID(),'-','')
 INSERT INTO school (school_id, school_name) values (UNHEX(REPLACE(UUID(),'-','')), '성결대학교');
 
 
-# 일반 유저 or 관리자
 INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_USER');
 INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_ADMIN');
