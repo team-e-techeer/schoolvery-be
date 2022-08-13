2aa977b8a8d967491f3d4c1ff9692c4fa34ecaf5
Ueibin <ats1113@zuminternet.com>
2022-08-13 21:11:30 +0900

    Update:Update application.properties in accesskey variable name

--- src/main/java/net/schoolvery/schoolveryserver/global/config/AwsS3Config.java
+++ src/main/java/net/schoolvery/schoolveryserver/global/config/AwsS3Config.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsS3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
