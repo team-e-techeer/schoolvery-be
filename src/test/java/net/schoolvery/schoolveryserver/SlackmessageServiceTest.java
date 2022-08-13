4cd1003c5c2f1e4c004321434c48b7bfcce54e92
Ueibin <ats1113@zuminternet.com>
2022-08-13 15:41:18 +0900

    DELETE: UserTest + change to slack API

--- src/test/java/net/schoolvery/schoolveryserver/SlackmessageServiceTest.java
+++ src/test/java/net/schoolvery/schoolveryserver/SlackmessageServiceTest.java
@@ -7,6 +7,7 @@ public class SlackmessageServiceTest {
 
     @Test
     void Send_message() {
-        SlackmessageConfig.send("Test Message");
+        SlackmessageConfig.send("현재 EC2 정상작동 중 http://52.79.69.132/swagger-ui/index.html#/ 스웨거 참조 + http://52.79.69.132:8082 그라파나 모니터링 확인가능!" );
+
     }
 }
